794202ee89795d7481903342fe1c48009c615a21
TERRY TANG (ITA-ISDC-ISD-OOCLL/ZHA) <TANGTE@OOCL.COM>
2019-07-11 09:54:34 +0800

    complete count same element number

--- practices/filter/grouping_count.js
+++ practices/filter/grouping_count.js
@@ -2,7 +2,22 @@
 
 function grouping_count(collection) {
 
-  //implement here
+  let copyNoSameElement = Array.from(new Set(collection));
+  let tempCollectionCountSameElement = {};
+  let collectionCountSameElement={};
+  copyNoSameElement.forEach(i => {
+    let count = 0;
+    collection.forEach(j => {
+      if (i === j)
+        count++;
+    });
+    tempCollectionCountSameElement[''+i+'']=count;
+  });
+  for(let k in tempCollectionCountSameElement){
+    collectionCountSameElement[''+k+'']=tempCollectionCountSameElement[k];
+  }
+  console.log(collectionCountSameElement);
+  return collectionCountSameElement;
 }
 
 module.exports = grouping_count;
